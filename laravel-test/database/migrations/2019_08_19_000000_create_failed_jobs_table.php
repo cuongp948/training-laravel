3f3ca461224357e748f343e30dcd2d9de19e69b7
cuongp948 <cuongp948>
2022-03-27 20:08:42 +0700

    a

--- laravel-test/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ laravel-test/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -31,6 +31,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('failed_jobs');
+        Schema::dropIfExists('failed_jobs');
     }
 }
